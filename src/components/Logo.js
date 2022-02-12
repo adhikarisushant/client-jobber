d48f52e35db989de1d4e0eec883878829a7fa746
adhikarisushant <sushant.adhikari.619@gmail.com>
2022-02-12 19:09:04 +0545

    Logo Component and imports

---
+++ src/components/Logo.js
@@ -0,0 +1,11 @@
+import logo from '../assets/images/logo.svg'
+
+const Logo = () => {
+    return (
+        <div>
+            <img src={logo} alt='jobber' className='logo' />
+        </div>
+    )
+}
+
+export default Logo
