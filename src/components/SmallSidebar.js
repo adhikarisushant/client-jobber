f9ec884e29f9e914521550dfa5832751c6a90ee8
adhikarisushant <sushant.adhikari.619@gmail.com>
2022-03-19 21:10:15 +0545

    small sidebar

--- src/components/SmallSidebar.js
+++ src/components/SmallSidebar.js
@@ -1,10 +1,37 @@
-import Wrapper from "../assets/wrappers/SmallSidebar"
+import Wrapper from '../assets/wrappers/SmallSidebar'
+import { FaTimes } from 'react-icons/fa'
+import { useAppContext } from '../context/appContext'
+import links from '../utils/links'
+import { NavLink } from 'react-router-dom'
+import Logo from './Logo'
 
 const SmallSidebar = () => {
+    const {showSidebar, toggleSidebar} = useAppContext()
     return (
         <Wrapper>
-            <h4>small sidebar</h4>
-        </Wrapper>
+        <div className={showSidebar ? 'sidebar-container show-sidebar' : 'sidebar-container' }>
+          <div className='content'>
+            <button className='close-btn' onClick={toggleSidebar}>
+              <FaTimes />
+            </button>
+            <header>
+              <Logo />
+            </header>
+            <div className='nav-links'>
+                {links.map((link) => {
+                    const {text, path, id, icon} =link
+                    
+                    return <NavLink to={path} key={id} onClick={toggleSidebar} className={({isActive}) => isActive ? 'nav-link active' : 'nav-link'}>
+                        <span className="icon">{icon}</span>
+                        {text}
+                    </NavLink>
+                    
+
+                })}
+            </div>
+          </div>
+        </div>
+      </Wrapper>
     )
 }
 
