ed5dd107fde4ebf3e0bb775f2198462d1e262348
adhikarisushant <sushant.adhikari.619@gmail.com>
2022-03-23 21:29:11 +0545

    Axios setup

--- src/context/appContext.js
+++ src/context/appContext.js
@@ -38,6 +38,37 @@ const  AppContext = React.createContext()
 
 const AppProvider = ({children}) => {
     const [state, dispatch] = useReducer(reducer,initialState)
+    
+    //axios 
+    const authFetch = axios.create({
+        baseURL:'/api/v1',
+    })
+
+    // request
+    authFetch.interceptors.request.use((config)=>{
+        config.headers.common['Authorization'] = `Bearer ${state.token}`
+        
+        return config
+    }, (error) => {
+        return Promise.reject(error)
+    })
+
+    // response
+    authFetch.interceptors.response.use(
+        (response) => {
+
+            return response
+        },
+        (error) => {
+            console.log(error.response);
+            if (error.response.status === 401) {
+                console.log('AUTH ERROR')
+            }
+            return Promise.reject(error)
+        }
+    )
+
+
 
     const displayAlert = () => {
         dispatch({ type:DISPLAY_ALERT })
@@ -126,7 +157,13 @@ const AppProvider = ({children}) => {
     }
 
     const updateUser = async (currentUser) => {
-        console.log(currentUser)
+        try {
+            const {data} = await authFetch.patch('/auth/updateUser', currentUser)
+
+            console.log(data)
+        } catch (error) {
+            console.log(error.response)
+        }
     }
 
     return (
