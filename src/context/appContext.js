83a262f992fb24790a23e0358a0da62264ea4359
adhikarisushant <sushant.adhikari.619@gmail.com>
2022-03-29 21:02:39 +0545

    job state values and add job page structure

--- src/context/appContext.js
+++ src/context/appContext.js
@@ -33,8 +33,16 @@ const initialState = {
     user: user? JSON.parse(user) : null,
     token: token,
     userLocation: userLocation || '',
-    jobLocation: userLocation || '',
     showSidebar: false,
+    isEditing:false,
+    editJobId:'',
+    position:'',
+    company:'',
+    jobLocation: userLocation || '',
+    jobTypeOptions: ['full-time', 'part-time', 'remote', 'internship'],
+    jobType: 'full-time',
+    statusOptions: ['interview', 'decline', 'pending'],
+    status:'pending'
 }
 
 const  AppContext = React.createContext()
--- src/pages/dashboard/AddJob.js
+++ src/pages/dashboard/AddJob.js
@@ -1,8 +1,82 @@
+import { FormRow, Alert } from '../../components'
+import { useAppContext } from '../../context/appContext'
+import Wrapper from '../../assets/wrappers/DashboardFormPage'
+
 const AddJob = () => {
+    const {
+            isEditing,
+            showAlert, 
+            displayAlert,
+            position, 
+            company, 
+            jobLocation, 
+            jobType, 
+            jobTypeOptions, 
+            status, 
+            statusOptions,
+        } = useAppContext()
+
+        const handleSubmit = e => {
+            e.preventDefault()
+
+            if(!position || !company || !jobLocation){
+                displayAlert()
+                return
+            }
+            console.log('create job')
+        }
+
+        const handleJobInput = (e) => {
+            const name = e.target.name
+            const value = e.target.value
+            console.log(`${name}: ${value}`);
+        }
+
     return (
-        <h1>
-            Add Job Page
-        </h1>
+        <Wrapper>
+            <form className='form'>
+                <h3>{isEditing ? 'edit job' : 'add job'}</h3>
+                {showAlert && <Alert />}
+                <div className="form-center">
+                    {/* position */}
+                    <FormRow 
+                        type="text" 
+                        name="position" 
+                        value={position} 
+                        handleChange={handleJobInput} 
+                    />
+
+                    {/* company */}
+                    <FormRow 
+                        type="text" 
+                        name="company" 
+                        value={company} 
+                        handleChange={handleJobInput} 
+                    />
+
+                    {/* location */}
+                    <FormRow 
+                        type="text"
+                        labelText="job location" 
+                        name="jobLocation" 
+                        value={jobLocation} 
+                        handleChange={handleJobInput} 
+                    />
+
+                    {/* job type */}
+                    {/* job status */}
+                    <div className="btn-container">
+                        <button 
+                            type="submit" 
+                            className="btn btn-block submit-btn"
+                            onClick={handleSubmit}    
+                        >
+                            submit
+                        </button>
+                    </div>
+                </div>
+            </form>
+        </Wrapper>
     )
 }
 
