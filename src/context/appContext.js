0ae9e30a56a7654fbea95e547e2ca8380e3f63f6
adhikarisushant <sushant.adhikari.619@gmail.com>
2022-04-05 17:15:31 +0545

    Edit Job Request

--- src/context/appContext.js
+++ src/context/appContext.js
@@ -28,6 +28,9 @@ import { DISPLAY_ALERT,
         GET_JOBS_SUCCESS,
         SET_EDIT_JOB,
         DELETE_JOB_BEGIN,
+        EDIT_JOB_BEGIN,
+        EDIT_JOB_SUCCESS,
+        EDIT_JOB_ERROR,
     } from './actions'
 
 const token = localStorage.getItem('token')
@@ -258,8 +261,30 @@ const AppProvider = ({children}) => {
         dispatch({ type: SET_EDIT_JOB, payload: { id } })
     }
 
-    const editJob = () => {
-        console.log('edit job');
+    const editJob = async () => {
+        dispatch({ type: EDIT_JOB_BEGIN })
+        try {
+            const { position, company, jobLocation, jobType, status } = state
+
+            await authFetch.patch(`/jobs/${state.editJobId}`,{
+                company,
+                position,
+                jobLocation,
+                jobType,
+                status,
+            })
+            dispatch({
+                type: EDIT_JOB_SUCCESS,
+            })
+            dispatch({ type: CLEAR_VALUES })
+        } catch (error) {
+            if(error.response.status === 401) return
+            dispatch({
+                type: EDIT_JOB_ERROR,
+                payload: {msg: error.response.data.msg },
+            })
+        }
+        clearAlert()
     }
 
     const deleteJob = async (jobId) =>{
--- src/context/reducer.js
+++ src/context/reducer.js
@@ -23,6 +23,9 @@ import { DISPLAY_ALERT,
         GET_JOBS_SUCCESS,
         SET_EDIT_JOB,
         DELETE_JOB_BEGIN,
+        EDIT_JOB_BEGIN,
+        EDIT_JOB_SUCCESS,
+        EDIT_JOB_ERROR,
     } from "./actions"
 
 import { initialState } from "./appContext"
@@ -254,8 +257,29 @@ const reducer = (state, action) => {
         }
     }
 
-    if (action.type === DELETE_JOB_BEGIN) {
+
+    if (action.type === EDIT_JOB_BEGIN) {
         return { ...state, isLoading: true }
+    }
+
+    if (action.type === EDIT_JOB_SUCCESS) {
+        return {
+          ...state,
+          isLoading: false,
+          showAlert: true,
+          alertType: 'success',
+          alertText: 'Job Updated!',
+        }
+    }
+
+    if (action.type === EDIT_JOB_ERROR) {
+        return {
+          ...state,
+          isLoading: false,
+          showAlert: true,
+          alertType: 'danger',
+          alertText: action.payload.msg,
+        }
     }
 
     throw new Error(`no such action :${action.type}`)
