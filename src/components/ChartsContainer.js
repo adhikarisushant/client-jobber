fcb616643e598a3c47eb2839cdfcd24e300facb5
adhikarisushant <sushant.adhikari.619@gmail.com>
2022-04-11 17:55:46 +0545

    Charts Monthly Applications

--- src/components/ChartsContainer.js
+++ src/components/ChartsContainer.js
@@ -1,8 +1,22 @@
+import React, { useState } from 'react'
+
+import BarChart from './BarChart'
+import AreaChart from './AreaChart'
+import { useAppContext } from '../context/appContext'
+import Wrapper from '../assets/wrappers/ChartsContainer'
+
 const ChartsContainer = () => {
+    const [barChart, setBarChart] = useState(true)
+    const { monthlyApplications: data } = useAppContext()
+
     return (
-        <div>
-            <h1>charts container</h1>
-        </div>
+        <Wrapper>
+            <h4>Monthly Applications</h4>
+            <button type='button' onClick={() => setBarChart(!barChart)}>
+                {barChart? 'AreaChart':'BarChart'}
+            </button>
+            { barChart ? <BarChart data={data} /> : <AreaChart data={data} /> }   
+        </Wrapper>
     )
 }
 
